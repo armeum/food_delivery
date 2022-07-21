b1595fac5fa2f4e6dfc729f3ff8c703b0297eb20
MunisaR <munisrahmatullayeva04@gmail.com>
2022-07-21 14:20:12 +0500

    commit

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"food_delivery/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgress"
+	password = "0990"
+	dbname = "database"
+) 
+
 func main() {
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgress", psqlconn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	insert := 
+
 	router := gin.New()
 	routes.UserRoute(router)
 	router.Run(":8080")
--- routes/user.go
+++ routes/user.go
@@ -2,22 +2,10 @@ package routes
 
 import (
 	"food_delivery/controllers"
-	"food_delivery/models"
 
 	"github.com/gin-gonic/gin"
 )
 
-type UserService interface {
-	CreateUser(*models.User) error
-	GetUser(*string) (*models.User, error)
-	GetAllUsers()([]*models.User, error)
-	UpdaterUser(*models.User)  error
-	DeleteUser(*string) error
-}
-
-
-func UserRoute(router *gin.Engine){
+func UserRoute(router *gin.Engine) {
 	router.GET("/", controllers.UserController)
-	}
-
-
+}
