501684cde098305d8b9ef7e5f7e4308d9618aa8e
MunisaR <munisrahmatullayeva04@gmail.com>
2022-07-26 15:18:32 +0500

    commit

--- models/product.go
+++ models/product.go
@@ -4,6 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Product struct {
 	gorm.Model
+	
 	Title        string `json:"name"`
 	Description string `json:"description"`
 	Price       string  `json:"price"`
