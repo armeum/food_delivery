f8ddea93adde14d40bbdcd1d723c6ca8b5445d36
MunisaR <munisrahmatullayeva04@gmail.com>
2022-07-27 15:41:38 +0500

    commit

---
+++ controllers/admin.go
@@ -0,0 +1,6 @@
+package controllers
+
+
+type AdminLogin struct {
+	PhoneNumber string `json:"phone_number"`
+}
--- models/admin.go
+++ models/admin.go
@@ -5,6 +5,7 @@ import "github.com/jinzhu/gorm"
 type Admin struct {
 	gorm.Model
 	Name string `json:"name"`
+	PhoneNumber string `json:"phone_number"`
 }
 
 
