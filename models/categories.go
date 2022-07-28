4c2cef74d6d6847ac9a901da7a37fb952815cf95
MunisaR <munisrahmatullayeva04@gmail.com>
2022-07-28 15:23:18 +0500

    commit

--- models/categories.go
+++ models/categories.go
@@ -1,10 +1,25 @@
 package models
 
-import "github.com/jinzhu/gorm"
+type Sales struct {
+	Product []Product `json:"product"`
+}
 
-type Categories struct {
-	gorm.Model
-	Name string `json:"name"`
-	Image string `json:"image"`
-	
+type Pizza struct {
+	Product []Product `json:"product"`
+}
+
+type Snacks struct {
+	Product []Product `json:"product"`
+}
+
+type Beverages struct {
+	Product []Product `json:"product"`
+}
+
+type Salads struct {
+	Product []Product `json:"product"`
+}
+
+type Deserts struct {
+	Product []Product `json:"product"`
 }
