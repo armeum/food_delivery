064fb49959539de8ae64d2f3d3e0319f4bf4acb0
MunisaR <munisrahmatullayeva04@gmail.com>
2022-07-28 12:19:37 +0500

    commit

--- controllers/basket.go
+++ controllers/basket.go
@@ -1,11 +1,27 @@
 package controllers
 
 import (
+	"food_delivery/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
 
-var db *gorm.DB
 
-func AddItemToBasket() {
-	
+func AddItemToBasket(c *gin.Context) {
+	//get model if exists
+	var item models.Item
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.Where("id = ?", c.Param("id")).Find(&item).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message":    "Route GET:/admin_products/:title not found",
+			"error":      "Record not found",
+			"statusCode": 404,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": item})
+
 }
