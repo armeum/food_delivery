9d48689973fb8846cffe24c6278c927811cc692c
MunisaR <munisrahmatullayeva04@gmail.com>
2022-08-11 21:52:10 +0500

    commit

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"headfirstgo/food_delivery/models"
 	"headfirstgo/food_delivery/routes"
 
+	"github.com/gin-contrib/cors"
 	_ "github.com/jinzhu/gorm"
 )
 
@@ -15,6 +16,14 @@ func main() {
 	db.AutoMigrate(&models.User{}, &models.Product{}, &models.Category{}, &models.Basket{}, &models.BasketItem{})
 
 	fmt.Println("Hello world!")
+
 	r := routes.UserRoutes(db)
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "OPTIONS"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true}))
+
 	r.Run(":8080")
 }
