45bdd8e874c1bdb38db7ad44df89f201b8d4ce97
MunisaR <munisrahmatullayeva04@gmail.com>
2022-08-23 17:06:04 +0500

    commit

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,8 +7,8 @@ import (
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-	fmt.Println("CORSMiddleware")
 	return func(c *gin.Context) {
+		fmt.Println("CORSMiddleware", c.Request)
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://delivery.umarakbarov.uz, http://localhost:8080, http://localhost")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
